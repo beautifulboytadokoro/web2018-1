7a89ff2ca6b127437e01df424a454ac845c2f7e3
zm17326 <zm17326@josai.ac.jp>
2018-08-08 11:27:02 -0400

    change table size

---
+++ ex0-1.js
@@ -0,0 +1,8 @@
+ function fruit1(){
+  document.getElementByid('myImage').src='./image/saru.png';}
+
+function fruit2(){
+  document.getElementByid('myImage').src='./image/sisi.png';}
+
+function fruit3(){
+  document.getElementByid('myImage').src='./image/inu.png';}
